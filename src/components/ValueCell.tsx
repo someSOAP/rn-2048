07259536d0ffc294a37eefcf5dd8f320fef035cf
someSOAP <andrey.meg@mail.ru>
2021-07-01 22:09:38 +0300

    basic megre animation

--- src/components/ValueCell.tsx
+++ src/components/ValueCell.tsx
@@ -1,97 +1,36 @@
 import React, { FC, useEffect, useRef } from 'react'
-import { Text, StyleSheet, Animated } from 'react-native'
+import { Text, StyleSheet, Animated, TouchableOpacity } from 'react-native'
 import { vw } from '@constants/window'
 import { CELL_DIMENSION } from '@constants/initail'
 import { ICell } from '@/types'
 import { getMoveOffset, mapColor } from '@utils/cell'
 
 const ValueCell: FC<ICell> = (cell) => {
-  const { value } = cell
+  const mergeAnim = useRef(new Animated.Value(0)).current
 
-  const animUp = useRef(new Animated.Value(0)).current
-  const animLeft = useRef(new Animated.Value(0)).current
-  const animDown = useRef(new Animated.Value(0)).current
-  const animRight = useRef(new Animated.Value(0)).current
-
-  const { offset, dir } = getMoveOffset(cell)
-
-  const startUp = () => {
-    Animated.timing(animUp, {
-      toValue: 100,
-      useNativeDriver: true,
-      duration: 250,
-    }).start()
-  }
-
-  const startLeft = () => {
-    Animated.timing(animLeft, {
-      toValue: 100,
-      useNativeDriver: true,
-      duration: 250,
-    }).start()
-  }
-
-  const startDown = () => {
-    Animated.timing(animDown, {
-      toValue: 100,
-      useNativeDriver: true,
-      duration: 250,
-    }).start()
-  }
-
-  const startRight = () => {
-    Animated.timing(animRight, {
-      toValue: 100,
-      useNativeDriver: true,
-      duration: 250,
-    }).start()
-  }
+  const { value, prevX, prevY, x, y, prevValue } = cell
 
   const style = Object.assign({}, styles.cell, mapColor(value), {
     transform: [
       {
-        translateX: animLeft.interpolate({
-          inputRange: [0, 100],
-          outputRange: [CELL_DIMENSION * offset, 0],
-        }),
-      },
-      {
-        translateY: animDown.interpolate({
-          inputRange: [0, 100],
-          outputRange: [-CELL_DIMENSION * offset, 0],
-        }),
-      },
-      {
-        translateX: animRight.interpolate({
-          inputRange: [0, 100],
-          outputRange: [CELL_DIMENSION * offset, 0],
-        }),
-      },
-      {
-        translateY: animUp.interpolate({
-          inputRange: [0, 100],
-          outputRange: [CELL_DIMENSION * offset, 0],
+        scale: mergeAnim.interpolate({
+          inputRange: [0, 75, 100],
+          outputRange: [1, 1.2, 1],
         }),
       },
     ],
   })
 
-  useEffect(() => {
-    if (!offset) return void 0
+  const animate = () => {
+    mergeAnim.setValue(0)
+    Animated.timing(mergeAnim, {
+      toValue: 100,
+      useNativeDriver: false,
+      duration: 250,
+    }).start()
+  }
 
-    if (dir === 'UP') {
-      startUp()
-    }
-    if (dir === 'DOWN') {
-      startDown()
-    }
-    if (dir === 'LEFT') {
-      startLeft()
-    }
-    if (dir === 'RIGHT') {
-      startRight()
-    }
-  })
+  useEffect(animate, [value, prevValue])
 
   return (
     <Animated.View style={style}>
