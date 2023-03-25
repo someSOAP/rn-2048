cbc3a9ca154ab5f14d8438040193cd44a64c00f6
Andrei Khavkunov <andrey.k@0hub.com>
2023-03-25 22:02:35 +0600

    app version update

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -78,7 +78,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.2)',
+    backgroundColor: 'rgba(0, 0, 0, 0.8)',
   },
   button: {
     width: 1.2 * CELL_DIMENSION,
